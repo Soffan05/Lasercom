777201c4614d8968fa939a53c277ed7058d268b3
Soffan05 <158750351+Soffan05@users.noreply.github.com>
2024-02-05 22:38:16 +0100

    Översättning från singnal till bokstav
    Nu har jag lyckats omvandla singnal till bokstäver. Nu ska jag bara lista ut hur man lägger in mellanslag. ps. kommetarer har lagts till i koden så att den är något läsbar :)

--- Lasercom.ino
+++ Lasercom.ino
@@ -5,27 +5,29 @@
 //Space between words = 7 Units
 
 
-
 #define LED 8
 #define sensor A0
 
 #include <string.h>
 
 //Variabler
-String WORD = "";
-int timing = 200;
-int i = 10;
+String WORD = ""; //
 
-//Variabler för ljussensorn
-int sens_state = 0;
-int count = 0;
-int garante = 0;
 
-//Variabler för översättning
-String encrypt;
+//Variabler
+int sens_state = 0; //Ljussensorns värde
+int count = 0; //Hur länge ljuset lyser i singnalen (kolla input funktionen)
+const int timing = 50; //En konstant timing för olika saker
+int Next_sing = 200; // Timingen när nästa bokstav ska komma
+
+String encrypt; //
 String translator;
+String decrypt;
+String result;
+String Morse_code = "";
+int i = 0; //Räknar timingen mellan singnal
 
-void dot() {
+void dot() { //Funktion till att blinka en DOT (Kort blinkning)
   Serial.print(" .");
   digitalWrite(LED, LOW);
   delay(500);
@@ -33,7 +35,7 @@ void dot() {
   delay(500);
 }
 
-void dash() {
+void dash() { //Funktion till att blinka en DASH (Lång blinkning)
   Serial.print(" -");
   digitalWrite(LED, LOW);
   delay(500);
@@ -41,89 +43,96 @@ void dash() {
   delay(500);
 }
 
-void translation(String encrypt) {
+String input(int sens_state) { //Funktion för att omvandla lasersingnal till kod
+  String encrypt = "";
+  while (true) {
+    do {
+      //FAS 1
+      count++;
+      sens_state = analogRead(sensor);
+      delay(timing);
+      //Serial.println(sens_state);
+    } while (sens_state < 700);
+
+    //FAS 2
+    //Serial.println(count);
+    switch (count) {
+      case 3:
+        encrypt += ".";
+        count = 0;
+        break;
+      case 5:
+        encrypt += "-";
+        count = 0;
+        break;
+      default:
+        Serial.println(encrypt);
+        count = 0;
+        break;
+    }
+    return encrypt; //RETURNING STRING
+    break;
+  }
+}
+
+
+String output(String encrypt) { //Funktion för att översätta morsekoden. Switch commandot funkade inte pga det gick inte att läga in sträningar i case. D: 
       if (encrypt == ".-") {
       translator = translator + "A";
-    }
-    if (encrypt == "-...") {
-      translator = translator + "B";
-    }
-    if (encrypt == "-.-.") {
-      translator = translator + "C";
-    }
-    if (encrypt == "-..") {
-      translator = translator + "D";
-    }
-    if (encrypt == ".") {
-      translator = translator + "E";
-    }
-    if (encrypt == "..-.") {
-      translator = translator + "F";
-    }
-    if (encrypt == "--.") {
-      translator = translator + "G";
-    }
-    if (encrypt == "....") {
-      translator = translator + "H";
-    }
-    if (encrypt == "..") {
-      translator = translator + "I";
-    }
-    if (encrypt == ".---") {
-      translator = translator + "J";
-    }
-    if (encrypt == "-.-") {
-      translator = translator + "K";
-    }
-    if (encrypt == ".-..") {
-      translator = translator + "L";
-    }
-    if (encrypt == "--") {
-      translator = translator + "M"; 
-    }
-    if (encrypt == "-.") {
-      translator = translator + "N";
-    }
-    if (encrypt == "---") {
-      translator = translator + "O";
-    }
-    if (encrypt == ".--.") {
-      translator = translator + "P";
-    }
-    if (encrypt == "--.-") {
-      translator = translator + "Q";
-    }
-    if (encrypt == ".-.") {
-      translator = translator + "R";
-    }
-    if (encrypt == "...") {
-      translator = translator + "S";
-    }
-    if (encrypt == "-") {
-      translator = translator + "T";
-    }
-    if (encrypt == "..-") {
-      translator = translator + "U";
-    }
-    if (encrypt == "...-") {
-      translator = translator + "v";
-    }
-    if (encrypt == ".--") {
-      translator = translator + "w";
-    }
-    if (encrypt == "-..-") {
-      translator = translator + "X";
-    }
-    if (encrypt == "-.--") {
-      translator = translator + "Y";
-    }
-    if (encrypt == "--..") {
-      translator = translator + "Z";
-    }
-    encrypt = "";
-    Serial.println(translator);
-
+      } else if (encrypt == "-...") {
+        translator = translator + "B";
+      } else if (encrypt == "-.-.") {
+        translator = translator + "C";
+      } else if (encrypt == "-..") {
+        translator = translator + "D";
+      } else if (encrypt == ".") {
+        translator = translator + "E";
+      } else if (encrypt == "..-.") {
+        translator = translator + "F";
+      } else if (encrypt == "--.") {
+        translator = translator + "G";
+      } else if (encrypt == "....") {
+        translator = translator + "H";
+      } else if (encrypt == "..") {
+        translator = translator + "I";
+      } else if (encrypt == ".---") {
+        translator = translator + "J";
+      } else if (encrypt == "-.-") {
+        translator = translator + "K";
+      } else if (encrypt == ".-..") {
+        translator = translator + "L";
+      } else if (encrypt == "--") {
+        translator = translator + "M"; 
+      } else if (encrypt == "-.") {
+        translator = translator + "N";
+      } else if(encrypt == "---") {
+        translator = translator + "O";
+      } else if (encrypt == ".--.") {
+        translator = translator + "P";
+      } else if (encrypt == "--.-") {
+        translator = translator + "Q";
+      } else if (encrypt == ".-.") {
+        translator = translator + "R";
+      } else if (encrypt == "...") {
+        translator = translator + "S";
+      } else if (encrypt == "-") {
+        translator = translator + "T";
+      } else if (encrypt == "..-") {
+        translator = translator + "U";
+      } else if (encrypt == "...-") {
+        translator = translator + "V";
+      } else if (encrypt == ".--") {
+        translator = translator + "W";
+      } else if (encrypt == "-..-") {
+        translator = translator + "X";
+      } else if (encrypt == "-.--") {
+        translator = translator + "Y";
+      } else if (encrypt == "--..") {
+        translator = translator + "Z";
+      }
+  return translator;
 }
+
 
 
 void setup() {
@@ -137,17 +146,15 @@ void setup() {
 
 void loop() {
 
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) { //Denna kommer aktiveras när man skriver något i Serial Monitor
     while (true) {
     
     
-    char Letter = Serial.read();
-
-    WORD += Letter;    
+    char Letter = Serial.read(); 
     Serial.print(Letter);
 
  
-    switch (Letter) {
+    switch (Letter) { //Här kollar programmet vilken bokstav som är på plats inom "The serial buffer".
       case 'a':
       case 'A':
         dot();
@@ -309,49 +316,61 @@ void loop() {
         dot();
         break;
       default:
-        Serial.println("Non lettern\n");
-        Serial.print(WORD);
+        Serial.println("Non lettern\n"); //Ingen bokstav
         break;
     
     }
     Serial.println("\n");
     break;
     } 
-    WORD = "";
   }
-      //FAS 1
-sens_state = analogRead(sensor);
 
-  if (sens_state < 700) {
-    while (true) {
-      while (sens_state < 700) {
-      count = count + 1;
-      Serial.println(count);
-      garante = count;
-      delay(timing);
-      sens_state = analogRead(sensor);
-      //FAS 2
-      if (sens_state > 700) {
-        if (garante == 2) {
-          encrypt = encrypt + ".";
-        } else if (garante == 4) {
-          encrypt = encrypt + "-";
+
+  sens_state = analogRead(sensor); //Ljussensorn kollar efter laserns ljus
+  //Serial.println(sens_state);
+
+  if (sens_state < 700) { //Här översätter programmet singnal till morsekod
+    while (i < Next_sing) {
+      i = 0;
+      sens_state = analogRead(sensor); //Kollar om lasern fortfarande lyser loopen 
+      encrypt = input(sens_state);
+      Morse_code += encrypt; // "." eller "-" ska inkluderas i morsekoden
+      Serial.println(Morse_code);
+    
+      while (sens_state > 700) { //Här räknar programmet ifall loopen ska stoppas eller fortsätta. Detta behövs för att programmet inte ska av mistag översätta morsekoden till en annan bokstav
+        i++; 
+        Serial.println(i);
+        sens_state = analogRead(sensor);
+        if (sens_state < 700) {
+          break;
+        } else if (i > Next_sing) {
+          break;
         }
-      Serial.println(encrypt);
-      break;
-        }
-      translation(encrypt);
       }
-    }
-    i = 0;
-    encrypt = "";
+    } 
+    Serial.println("NEXT"); //Översättningen från singal till morsekod är klar
   }
-  
-  count = 0;
-  garante = 0;
 
+  if (i > Next_sing) { //Här översätter programmet morsekoden till bokstäver och lägger in dem i en sträng variabel "result"
+    decrypt = output(Morse_code); //Funktionen retunerar en bokstav i variabeln "decrypt"
+    Morse_code = ""; //Morsekoden börjar om
+    //Serial.println(decrypt);
+    result += decrypt; //Den översättande bokstaven lägs in i resultatet
+    Serial.println(result);
+    Serial.println("DONE"); //Översättningen är klar
+    
+  }
 
+  i = 0;
 
 
 }
+
+    
+
+  
+
+
+
+
 
